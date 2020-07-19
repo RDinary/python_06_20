9861c5473fb7cebaa4242fc95026119e30063373
Rafi Dinari <rafidinary@gmail.com>
2020-07-19 09:43:17 +0300

    template for homework 1

---
+++ HW01_template.py
@@ -0,0 +1,45 @@
+def Max(arr):
+    pass
+
+
+def Min(arr):
+    pass
+
+
+def Add(arr, value):
+    pass
+
+
+def Remove(arr, value):
+    pass
+
+
+def Sort(arr, asc):
+    '''
+    :param arr: array to be sorted
+    :param asc: bool. if true sort by ascending order, else by descending order
+    '''
+    pass
+
+
+def Average(arr):
+    pass
+
+
+def Index(value):
+    pass
+
+
+def Count(value):
+    pass
+
+
+def missing_numbers(arr):
+    pass
+
+
+def refill(arr):
+    '''
+    insert 500 random numbers to arr
+    '''
+    pass
